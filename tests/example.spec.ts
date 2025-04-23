4c2045feff353bcaa3d1ea0a849576bd4c18c3b6
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2025-04-23 20:55:02 -0300

    playwright init

---
+++ tests/example.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+
+test('getByRole "form" fails', async ({ page }) => {
+  await page.goto('https://semantic-ui.com/collections/form.html');
+
+  await expect(page.getByRole('form').first()).toBeVisible()
+});
+
+
+test('locator "form" works', async ({ page }) => {
+  await page.goto('https://semantic-ui.com/collections/form.html');
+
+  await expect(page.locator('form').first()).toBeVisible()
+});
+
+test('getByLabel without any attribute fails', async ({ page }) => {
+  await page.goto('https://semantic-ui.com/collections/form.html');
+
+  await expect(page.getByLabel('First Name')).toBeVisible()
+});
+
+test('getByText works', async ({ page }) => {
+  await page.goto('https://semantic-ui.com/collections/form.html');
+
+  await expect(page.getByText('First Name').first()).toBeVisible()
+});
